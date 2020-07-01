f58cb6c8213053e355165bd98d3f9dbd018192af
Priyanshu Vishnoi <priyanshuvishnoi990@gmail.com>
2020-07-01 12:29:19 +0530

    User Authentication with Passport

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -6,6 +6,8 @@ const logger = require('morgan');
 const mongoose = require('mongoose');
 const session = require('express-session');
 const FileStore = require('session-file-store')(session);
+const passport = require('passport');
+const authenticate = require('./authenticate');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -47,25 +49,18 @@ app.use(
 	})
 );
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 function auth(req, res, next) {
-	console.log(req.session);
-
-	if (!req.session.user) {
+	if (!req.user) {
 		const err = new Error('You are not authenticated!');
 		err.status = 403;
 		return next(err);
-	} else {
-		if (req.session.user === 'authenticated') {
-			next();
-		} else {
-			const err = new Error('You are not authenticated!');
-			err.status = 403;
-			return next(err);
-		}
-	}
+	} else next();
 }
 
 app.use(auth);
